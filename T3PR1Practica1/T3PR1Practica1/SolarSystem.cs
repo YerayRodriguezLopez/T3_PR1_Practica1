ef4bfb7b3c51594b12de4c964ef1ee53abadc855
YerayRodriguez <yeray.rodriguez.7e5@itb.cat>
2025-01-23 13:19:45 +0100

    Initial commit, project, diagram and cs files created

---
+++ T3PR1Practica1/T3PR1Practica1/SolarSystem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T3PR1Practica1
+{
+    public class SolarSystem : EnergySystem
+    {
+    }
+}
